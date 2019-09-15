210959c05c370b1a5c94cc0369f7510645eed03f
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-15 03:41:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+let mealId = 0;
+let customerId = 0;
+let deliveryId = 0;
+
+class Neighborhood {
+  constructor(name) {
+      this.id = ++neighborhoodId;
+      this.name = name;
+      store.neighborhoods.push(this);
+  }
